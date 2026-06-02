dc82552d3556eeb700c24f7d501a8a66d84059fa
andy <andyt.cyb@gmail.com>
2026-06-02 10:11:20 -0400

    ans

--- Excercise.sql
+++ Excercise.sql
@@ -6,14 +6,25 @@ SELECT * FROM suppliers;
 
 -- 2. What is the sum of all the employee's salaries?
 
-SELECT salary FROM employees;
+SELECT SUM(Salary) FROM employees;
+
 -- 3. What is the price of the cheapest item that Northwind sells?
+
+SELECT productname, unitprice FROM products
+ORDER BY unitprice ASC
+LIMIT 1;
 -- 4. What is the average price of items that Northwind sells?
+SELECT AVG(unitprice) FROM products;
 -- 5. What is the price of the most expensive item that Northwind sells?
+SELECT MAX(unitprice) FROM products;
 -- 6. What is the supplier ID of each supplier and the number of items they supply?
 -- You can answer this query by only looking at the Products table.
+SELECT supplierid, quantityperunit FROM products;
 -- 7. What is the category ID of each category and the average price of each item in the
 -- category? You can answer this query by only looking at the Products table.
+
+SELECT categoryid, AVG(unitprice) FROM products
+GROUP BY categoryid;
 -- 8. For suppliers that provide at least 5 items to Northwind, what is the supplier ID of
 -- each supplier and the number of items they supply? You can answer this query
 -- by only looking at the Products table.
